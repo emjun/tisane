afb8b172c94a6911905306fe7f2cdda4ab6f616f
Eunice Jun <eunice.m.jun@gmail.com>
2020-12-02 16:53:44 -0800

    Add correlation test case

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -97,6 +97,38 @@ class AnalysisTests(unittest.TestCase):
         self.assertIsNone(analysis.relationship)
         self.assertIsNone(analysis.data)
 
+    def test_addRelationships_correlate(self): 
+        analysis = ts.Tisane(task="prediction") # analysis has one task
+
+        test_score = ts.Concept("Test Score")
+        intelligence = ts.Concept("Intelligence")
+        tutoring = ts.Concept("Tutoring")
+        concepts = [test_score, intelligence, tutoring]
+
+        # ASSERT 
+        self.assertTrue(nx.is_empty(analysis.graph._graph))
+
+        analysis.addRelationship(intelligence, test_score, "correlate")
+        analysis.addRelationship(tutoring, test_score, "correlate")
+
+        # ASSERT
+        self.assertEqual(analysis.task,TASK.cast("prediction"))
+        for con in concepts: 
+            self.assertTrue(analysis.graph.hasConcept(con))
+
+        self.assertTrue(analysis.graph._graph.has_edge(intelligence, test_score))
+        edge_info = analysis.graph._graph.get_edge_data(intelligence, test_score)
+        edge_type = edge_info[0]['edge_type']
+        self.assertEqual(edge_type, CONCEPTUAL_RELATIONSHIP.CORRELATION)
+        
+        self.assertTrue(analysis.graph._graph.has_edge(tutoring, test_score))
+        edge_info = analysis.graph._graph.get_edge_data(tutoring, test_score)
+        edge_type = edge_info[0]['edge_type']
+        self.assertEqual(edge_type, CONCEPTUAL_RELATIONSHIP.CORRELATION)
+        
+        self.assertIsNone(analysis.relationship)
+        self.assertIsNone(analysis.data)
+
 class DataForTests:
     concepts_to_define = [
         {"name": "NominalT", "data type": "nominal", "categories": ["Nominal0"], "data": ["Val1", "Val2", "Val3"]},
