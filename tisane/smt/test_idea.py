7a21c64a98b000754e0e8b6bdbf5aea96688158c
Eunice Jun <eunice.m.jun@gmail.com>
2021-02-04 22:08:53 -0800

    WIP. Initial structure of interactive/incremental solving.

--- tisane/smt/test_idea.py
+++ tisane/smt/test_idea.py
@@ -33,6 +33,7 @@ model_explanation = [
 
 Cause = Function('Cause', Object, Object, BoolSort())
 Correlate = Function('Correlate', Object, Object, BoolSort())
+Cause_or_Correlate = Function('Cause_or_Correlate', Object, Object, BoolSort())
 HasEdge = Function('HasEdge', Object, Object, BoolSort())
 
 conceptual_graph = [
@@ -41,38 +42,80 @@ conceptual_graph = [
     # ForAll([x, y], Implies(HasEdge(x, y), (Or(Cause(x, y), Correlate(x, y))))),
     # Implies(Cause(x,y), HasEdge(x, y)),
     # Implies(Correlate(x,y), HasEdge(x, y)),
+    
+    # Implies(Cause_or_Correlate(x, y), ForAll([x, y], Xor(Correlate(x, y), Cause(x, y)))),
     ForAll([x, y], Xor(Correlate(x, y), Cause(x, y))),
     # Xor(Cause(x, y), Correlate(x, y)),
 ]
 
 
-axioms = model_explanation + conceptual_graph
 s = Solver()
-s.add(axioms)
-print(s.check())
 
-
-# objects to test with 
+# Create objects to test with 
 sat = Const('sat', Object)
 intelligence = Const('intelligence', Object)
 tutoring = Const('tutoring', Object)
-
 # Create list of IVs
 ivs = List.cons(tutoring, List.nil)
 ivs = List.cons(intelligence, ivs) 
-# Create sequence of IVs
-# ivs = SeqSort(tutoring, intelligence)
+
+# Add model
 s.add(Models(sat, ivs))
 
-# Create edge
-# s.add(HasEdge(tutoring, sat))
-s.add(Cause(tutoring, sat))
-s.add(Correlate(tutoring, sat))
+# Add constraints
+s.add(And(model_explanation))
 
-print(s.assertions())
+if (s.check() == unsat): 
+    import pdb; pdb.set_trace()
+s.push()
+
+# Add user assertions
+input_conceptual_graph_assertions = [
+    Cause(tutoring, sat), 
+    Correlate(tutoring, sat)
+]
+# s.add(Cause(tutoring, sat))
+# s.add(Correlate(tutoring, sat))
+s.add(input_conceptual_graph_assertions)
+
+# Add constraints
+s.add(And(conceptual_graph))
+s.push()
+
+if (s.check() == unsat): 
+    s.pop()
+
+    s.push()
+    # Iterate over each constraint in the chunk that caused an issue.
+    for c in conceptual_graph: 
+        s.add(c)
+        if (s.check() == unsat): 
+            print(s.unsat_core()) # TODO: toy test problem where we get to add multiple tests incrementally and then see which combo leads to UNSAT and use unsat_core (similar to example)
+            import pdb; pdb.set_trace()
+            print(f"{c}")
+            s.pop() # remove this (single) constraint 
+            s.pop() # go back once more and remove user-input constraints, there might be an issue there. 
+            
+            s.add(c) # add this constraint back in
+            # Try each user-input constraint one at a time
+            for a in input_conceptual_graph_assertions: 
+                s.add(a)
+               
+                if (s.check() == unsat): 
+                    try: 
+                        val = input(f'Can you clarify {c}, {a}:')
+                    except ValueError: 
+                        print("Try again.")
+                        continue
+                    else: 
+                        break
+
+            
+            # print(s.assertions())
+    import pdb; pdb.set_trace()
+    
 print(s.check()) 
-
-# print(s.model())
+print(s.model()) # assumes s.check() is SAT
 
 
 # ts = Solver()
