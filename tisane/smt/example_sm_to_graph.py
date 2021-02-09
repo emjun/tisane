2c40403e034549df59c5a97e2c563535f2b0beaf
Eunice Jun <eunice.m.jun@gmail.com>
2021-02-09 11:24:20 -0800

    Add data type for interaction and rule

--- tisane/smt/example_sm_to_graph.py
+++ tisane/smt/example_sm_to_graph.py
@@ -3,14 +3,19 @@ from z3 import *
 
 s = Solver()
 # s.set("core.minimize",True) # force min unsat core, may not need
-# TODO: Interaction effects 
 
 # Declare data type
 Object = DeclareSort('Object')
+
 # Declare free variables 
 x = Const('x', Object)
+x0 = Const('x0', Object)
+x1 = Const('x1', Object)
 
-##### MAIN EFFECTS ONLY
+# i0 = Const('i0', Object)
+
+
+##### MAIN EFFECTS ONLY : Y = X1 + X2
 # Create objects for current statistical model
 sat_score = Const('sat_score', Object)
 _y = sat_score
@@ -19,7 +24,11 @@ tutoring = Const('tutoring', Object)
 
 # Create sequence of main effects
 main_effects = Concat(Unit(tutoring), Unit(intelligence))
-# interactions = Unit(IntVal(0))
+# interactions = pair((first tutoring), (second intelligence))
+interactions = EmptySet(Object)
+interactions = SetAdd(interactions, tutoring)
+interactions = SetAdd(interactions, intelligence)
+
 
 # Declare functions for properties pertaining to model explanation
 Explains = Function('Explains', Object, SeqSort(Object), BoolSort())
@@ -38,19 +47,25 @@ Correlate = Function('Correlate', Object, Object, BoolSort())
 
 rules = [
     ForAll([x], Implies(Contains(main_effects, Unit(x)), Xor(Cause(x, _y), Correlate(x, _y)))),
+    # ForAll([x0, x1], Implies(Interaction(x0, x1), Xor(Cause(x0, x1), Correlate(x0, x1)))),
+    # Implies(Interaction(x0, x1), Xor(Cause(x0, x1), Correlate(x0, x1))),
+    ForAll([x0, x1], Implies(And(IsMember(x0, interactions), IsMember(x1, interactions)), Xor(Cause(x0, x1), Correlate(x0, x1))))
+    # ADD RULE FOR CANT INTERACTION X with Y
+
     # ForAll([x], Implies(NoInteractions(_y, interactions), Length(interactions) == 1))
     # ForAll([xs], Implies(Contains(interactions, Unit(xs)), Xor(Cause(x, sat_score), Correlate(x, sat_score)))),
     # Something similar for Between vs. Within subjects?
     # Between subjects means occurs once per subject   
 ]
 
+# starting state
 facts = [
     Models(sat_score, main_effects), 
     Dependent(sat_score),
-    MainEffect(tutoring),
+    # MainEffect(tutoring),
     Cause(tutoring, sat_score), # Add both for each main effect and require end-user to pick one
     Correlate(tutoring, sat_score),
-    MainEffect(intelligence),
+    # MainEffect(intelligence),
     Cause(intelligence, sat_score), # Add both for each main effect and require end-user to pick one
     Correlate(intelligence, sat_score),
 ]
@@ -58,6 +73,7 @@ facts = [
 # At end of facts disambiguation, will have a complete set of facts for conceptual graph
 # TODO: If add more facts about additional relationships (e.g., that might suggest interaction), what do we do? -- this is something we would have to take care of in sys architecture?
 # TODO: START HERE: Read advanced webpage (incremental)
+# TODO: Need to know which facts to add depending on query!!!
 # Create separate sample programs for different queries and then figure out how to generate dynamically/connect up with rest of Tisane
 # Query for variable relationship graph
 # Query for data schema
@@ -68,9 +84,23 @@ s = check_update_constraints(solver=s, assertions=facts)
 # Output 
 # Create Variable Relationship Graph
 parse_and_create_variable_relationship_graph(solver=s)
-
-
-# TODO: Need to know which facts to add depending on query!!!
-
 # s.consequences()
 #s.eval()
+
+
+##### INTERACTION EFFECTS : Y = X1 + X2 + X1*X2
+# Need to add when instantiate interactions variable above, before rules using interactions
+# interactions = SetAdd(interactions, tutoring)
+# interactions = SetAdd(interactions, intelligence)
+
+ixn_facts = [
+    Models(sat_score, main_effects), 
+    Dependent(sat_score),
+    Cause(tutoring, intelligence), # Add both for each main effect and require end-user to pick one
+    Correlate(tutoring, intelligence),
+]
+
+s1 = Solver()
+s1.add(rules)
+s1 = check_update_constraints(solver=s1, assertions=ixn_facts)
+parse_and_create_variable_relationship_graph(solver=s1)
