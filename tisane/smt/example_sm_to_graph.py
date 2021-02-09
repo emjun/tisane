6ecabf33c33e60e328e42fa8cbeba74a66470858
Eunice Jun <eunice.m.jun@gmail.com>
2021-02-09 14:04:00 -0800

    Update interactions data type to be a sequence of sets of interactions (of arbitrary length)

--- tisane/smt/example_sm_to_graph.py
+++ tisane/smt/example_sm_to_graph.py
@@ -11,9 +11,7 @@ Object = DeclareSort('Object')
 x = Const('x', Object)
 x0 = Const('x0', Object)
 x1 = Const('x1', Object)
-
-# i0 = Const('i0', Object)
-
+i = Const('x1', SetSort(Object))
 
 ##### MAIN EFFECTS ONLY : Y = X1 + X2
 # Create objects for current statistical model
@@ -22,13 +20,20 @@ _y = sat_score
 intelligence = Const('intelligence', Object)
 tutoring = Const('tutoring', Object)
 
+
 # Create sequence of main effects
 main_effects = Concat(Unit(tutoring), Unit(intelligence))
-# interactions = pair((first tutoring), (second intelligence))
-interactions = EmptySet(Object)
-interactions = SetAdd(interactions, tutoring)
-interactions = SetAdd(interactions, intelligence)
 
+# Create set for interaction
+tutoring_intelligence = EmptySet(Object)
+tutoring_intelligence = SetAdd(tutoring_intelligence, tutoring)
+tutoring_intelligence = SetAdd(tutoring_intelligence, intelligence)
+interactions = Unit(tutoring_intelligence)
+
+tutoring_sat = EmptySet(Object)
+tutoring_sat = SetAdd(tutoring_sat, tutoring)
+tutoring_sat = SetAdd(tutoring_sat, sat_score)
+interactions = Concat(Unit(tutoring_sat), interactions)
 
 # Declare functions for properties pertaining to model explanation
 Explains = Function('Explains', Object, SeqSort(Object), BoolSort())
@@ -49,7 +54,8 @@ rules = [
     ForAll([x], Implies(Contains(main_effects, Unit(x)), Xor(Cause(x, _y), Correlate(x, _y)))),
     # ForAll([x0, x1], Implies(Interaction(x0, x1), Xor(Cause(x0, x1), Correlate(x0, x1)))),
     # Implies(Interaction(x0, x1), Xor(Cause(x0, x1), Correlate(x0, x1))),
-    ForAll([x0, x1], Implies(And(IsMember(x0, interactions), IsMember(x1, interactions)), Xor(Cause(x0, x1), Correlate(x0, x1))))
+    ForAll([x0, x1, i], Implies(And(Contains(interactions, Unit(i)), And(IsMember(x0, i), IsMember(x1, i))), Xor(Cause(x0, x1), Correlate(x0, x1)))),
+    # ForAll([x0, x1], Implies(And(IsMember(x0, interactions), IsMember(x1, interactions)), Xor(Cause(x0, x1), Correlate(x0, x1))))
     # ADD RULE FOR CANT INTERACTION X with Y
 
     # ForAll([x], Implies(NoInteractions(_y, interactions), Length(interactions) == 1))
@@ -72,7 +78,6 @@ facts = [
 
 # At end of facts disambiguation, will have a complete set of facts for conceptual graph
 # TODO: If add more facts about additional relationships (e.g., that might suggest interaction), what do we do? -- this is something we would have to take care of in sys architecture?
-# TODO: START HERE: Read advanced webpage (incremental)
 # TODO: Need to know which facts to add depending on query!!!
 # Create separate sample programs for different queries and then figure out how to generate dynamically/connect up with rest of Tisane
 # Query for variable relationship graph
@@ -96,7 +101,11 @@ parse_and_create_variable_relationship_graph(solver=s)
 ixn_facts = [
     Models(sat_score, main_effects), 
     Dependent(sat_score),
-    Cause(tutoring, intelligence), # Add both for each main effect and require end-user to pick one
+    Cause(tutoring, sat_score), # main effect disambiguation
+    Correlate(tutoring, sat_score),
+    Cause(intelligence, sat_score), # main effect disambiguation
+    Correlate(intelligence, sat_score),
+    Cause(tutoring, intelligence), # Add both for each interaction effect and require end-user to pick one
     Correlate(tutoring, intelligence),
 ]
 
