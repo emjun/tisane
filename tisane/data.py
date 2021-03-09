2b4c30b36b432ce1547203fb846f23b0fce585f6
Eunice Jun <eunice.m.jun@gmail.com>
2021-03-09 10:26:46 -0800

    Add to Data class

--- tisane/data.py
+++ tisane/data.py
@@ -1,8 +1,19 @@
+import os
 import pandas as pd 
+from typing import Union
 
 class Dataset(object): 
-    data_vectors: dict
+    # data_vectors: dict
+    data : pd.DataFrame
 
+    # Takes input in either a CSV or a Pandas DataFrame
+    def __init__(self, source: Union[str, pd.DataFrame]): 
+        df = None
+        if isinstance(source, str): 
+            df = pd.read_csv(source)
+
+        if isinstance(source, pd.DataFrame): 
+            data = source
 
 class DataVector(object): 
     name: str
