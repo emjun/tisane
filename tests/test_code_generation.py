4c1c3d18bddfa6cc05449924cc51ae68485efd6c
Eunice Jun <eunice.m.jun@gmail.com>
2021-03-25 14:29:52 -0700

    Debug. Minor edits to test cases

--- tests/test_code_generation.py
+++ tests/test_code_generation.py
@@ -4,7 +4,10 @@ from tisane.code_generator import *
 from z3 import *
 import pandas as pd
 import numpy as np
+import random
 import unittest
+
+random.seed(2)
 
 class TestCodeGeneration(unittest.TestCase): 
     def test_generate_slr(self): 
@@ -14,22 +17,14 @@ class TestCodeGeneration(unittest.TestCase):
         hw = ts.Numeric('HomeWork')
         race = ts.Nominal('Race')
 
-        df =  pd.DataFrame({
-                'MathAchievement': np.random.randn(50),
-                'HomeWork': np.random.randn(50)+1},
-                # 'Race': ,
-                # 'SES': ,
-                # 'MeanSES': 
-            )
-
         sm = ts.StatisticalModel(
             dv=math,
             fixed_ivs=[hw], 
             random_ivs=None, 
             interactions=None
-        ).assign_data(df)
+        ).assign_data(DataForTests.df)
         
-        script = generate_statsmodel_code(sm)
+        script = generate_statsmodel_code(sm, file_name='slr_model.py')
         
         # Add test case about script that is generated.
         with open(script, 'r') as f: 
@@ -40,17 +35,45 @@ class TestCodeGeneration(unittest.TestCase):
         # Student-level variables 
         student = ts.Nominal('id')
         hw = ts.Numeric('HomeWork')
+        ses = ts.Numeric('SES')
         race = ts.Nominal('Race')
 
         sm = ts.StatisticalModel(
             dv=math,
-            fixed_ivs=[hw, race], 
+            fixed_ivs=[hw, ses], 
             random_ivs=None, 
             interactions=None
-        )
+        ).assign_data(DataForTests.df)
 
+        script = generate_statsmodel_code(sm, file_name='mlr_model.py')
+        
+        # Add test case about script that is generated.
+        with open(script, 'r') as f: 
+            pass
+        
     def test_generate_slr_with_interaction(self): 
         pass
 
     def test_generate_mlr_with_interaction(self): 
         pass
+
+class DataForTests(): 
+    def random_float_list(lb: int, ub: int, num: int): 
+        rlist = list() 
+
+        for i in range(num): 
+            val = ub*random.random() + lb
+            rlist.append(val)
+        
+        assert(len(rlist) == num)
+        return rlist
+
+    math = random_float_list(lb=0, ub=101, num=50)
+    df =  pd.DataFrame({
+                'MathAchievement': math,
+                'HomeWork': np.random.randint(0, 24, 50),
+                'SES': np.random.randint(10000, 150000, 50)}
+                # 'Race': ,
+                # 'SES': ,
+                # 'MeanSES': 
+            )
