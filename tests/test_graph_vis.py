fff8d066e88709f8b5e71d5f80ebfecc76a3a5ad
Eunice Jun <eunice.m.jun@gmail.com>
2021-08-10 10:32:00 -0700

    Add output from test graph vis

--- tests/test_graph_vis.py
+++ tests/test_graph_vis.py
@@ -17,53 +17,53 @@ class GraphVisTest(unittest.TestCase):
     #     gr = ts.Graph()
     #     gr._add_variable(v1)
     #     gr._add_variable(v2)
-    #     gr.cause(v1, v3)
-    #     gr.cause(v2, v3)
+    #     gr.causes(v1, v3)
+    #     gr.causes(v2, v3)
     #
     #     gr.visualize_graph
     #     self.assertIsInstance(v1, ts.Nominal)
-    def test_units(self):
-        student = ts.Unit("student id")
-        school = ts.Unit("school id")
-        test_score = student.numeric("test score")
+    # def test_units(self):
+    #     student = ts.Unit("student id")
+    #     school = ts.Unit("school id")
+    #     test_score = student.numeric("test score")
 
-        gr = ts.Graph()
-        gr._add_variable(student)
-        gr._add_variable(school)
-        gr._add_variable(test_score)
-        school.causes(test_score)
-        # gr.has(student, test_score)
-        gr.cause(school, test_score)
-        gr.associate(student, test_score)
+    #     gr = ts.Graph()
+    #     gr._add_variable(student)
+    #     gr._add_variable(school)
+    #     gr._add_variable(test_score)
+    #     school.causes(test_score)
+    #     # gr.has(student, test_score)
+    #     gr.causes(school, test_score)
+    #     gr.associates_with(student, test_score)
 
-        gr._get_graph_tikz()
+    #     gr._get_graph_tikz()
 
-        # gr.visualize_graph()
+    #     # gr.visualize_graph()
 
-    def test_more_complex(self):
+    # def test_more_complex(self):
 
-        student = ts.Unit(
-            "Student", attributes=[]
-        )  # object type, specify data types through object type
-        race = student.nominal("Race", cardinality=5, exactly=1)  # proper OOP
-        ses = student.numeric("SES")
-        test_score = student.nominal("Test score")
-        tutoring = student.nominal("treatment")
+    #     student = ts.Unit(
+    #         "Student", attributes=[]
+    #     )  # object type, specify data types through object type
+    #     race = student.nominal("Race", cardinality=5, exactly=1)  # proper OOP
+    #     ses = student.numeric("SES")
+    #     test_score = student.nominal("Test score")
+    #     tutoring = student.nominal("treatment")
 
-        race.associates_with(test_score)
-        student.associates_with(test_score)
-        race.causes(tutoring)
+    #     race.associates_with(test_score)
+    #     student.associates_with(test_score)
+    #     race.causes(tutoring)
 
-        race.moderate(ses, on=test_score)
+    #     race.moderates(ses, on=test_score)
 
-        design = ts.Design(dv=test_score, ivs=[race, ses])
-        print(design.get_variables())
+    #     design = ts.Design(dv=test_score, ivs=[race, ses])
+    #     print(design.get_variables())
 
-        gr = design.graph
-        print(gr.get_nodes())
-        gr._get_graph_tikz()
-        # self.assertTrue(gr.has_variable(test_score))
-        gr.visualize_graph()
+    #     gr = design.graph
+    #     print(gr.get_nodes())
+    #     gr._get_graph_tikz()
+    #     # self.assertTrue(gr.has_variable(test_score))
+    #     gr.visualize_graph()
 
     # As of August 10, this is the example used in the README
     def test_exercise_group_simplified(self):
@@ -84,4 +84,26 @@ class GraphVisTest(unittest.TestCase):
 
         design = ts.Design(dv=pounds_lost, ivs=[age, race, week])
         gr = design.graph
-        gr.visualize_graph()
+        gr._get_graph_tikz()
+
+#         \begin{tikzpicture}[cause/.style={draw=red, "c", text=red},
+#                     associate/.style={draw=black},
+#                     min/.style={minimum size=2cm},
+#                     unit/.style={min,draw=black},
+#                     measure/.style={min,circle,draw=black}]
+
+#     \graph[spring layout,sibling distance=3cm,level distance=3cm]{
+# 		member[unit] -> [has] pounds_lost[measure];
+# 		member -> [has] age[measure];
+# 		member -> [has] age*motivation[measure];
+# 		member -> [has] race group[measure];
+# 		pounds_lost -> [associates] race group;
+# 		pounds_lost -> [associates] Week[measure];
+# 		pounds_lost -> [associates] age*motivation;
+# 		motivation[measure] -> [causes] pounds_lost;
+# 		race group -> [associates] pounds_lost;
+# 		Week -> [associates] pounds_lost;
+# 		age*motivation -> [associates] pounds_lost;
+
+#     };
+# \end{tikzpicture}
