aa0abec3c58a5a5b216bf36efd045a7e6cdcbe42
Audrey Seo <audreyleeseo@gmail.com>
2021-09-01 21:28:07 -0700

    Fix group exercise with age added
    The path for the data got messed up

--- examples/Group_Exercise/exercise_group_age_added.py
+++ examples/Group_Exercise/exercise_group_age_added.py
@@ -5,23 +5,23 @@ import numpy as np
 import os
 
 """
-Example from Cohen, Cohen, West, Aiken 2003. 
+Example from Cohen, Cohen, West, Aiken 2003.
 
 Background, from Cohen et al. 2003: We assume that the data have been collected
 from intact women's groups that have a focus on diet and weight control; the
 groups meet regularly to discuss diet and weight control, and have some level of
 cohesion. we may thus expect some correlation among the women within a group j
-in both their motivation to lose weight and weight loss success. 
+in both their motivation to lose weight and weight loss success.
 
 There are a total of 386 women in all distributed across the 40 groups. Group
 size ranges from 5 to 15 women. There is substantial clustering in the data,
 reflected in the fact that the groups differe substantially in mean pounds of
 weight lost, from a low mean of 9.75 points lost to a high mean of 24.43 pounds
-lost. 
+lost.
 """
 
 # Load data
-data_path = os.path.join("/examples/Group_Exercise/", "exercise_group_age_added.csv")
+data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "exercise_group_age_added.csv")
 
 # Variable declarations
 adult = ts.Unit("member", cardinality=386)
